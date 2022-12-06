2187757825300617b41e42fb9b4d02f67f21936d
Kondrateva Aelina <aelina.kondrateva@mail.ru>
2022-12-06 20:44:40 +0300

    editing

--- search_engine.py
+++ search_engine.py
@@ -1,5 +1,4 @@
 import pickle
-
 import PIL.Image
 import cv2 as cv
 import pandas as pd
@@ -12,10 +11,7 @@ import PIL
 @st.cache(allow_output_mutation=True)
 def load_model():
     loaded_model = pickle.load(open('pic_model.sav', 'rb'))
-    # mp='./data/clf.pkl'
-    # _clf = pickle.load(open(mp,'rb'))
     return loaded_model
-
 
 @st.cache(allow_output_mutation=True)
 def load_bases():
@@ -26,7 +22,6 @@ def load_bases():
     vec_base['embedding'] = vec_base['embedding'].apply(
         lambda x: np.frombuffer(base64.b64decode(bytes(x[2:-1], encoding='ascii')), dtype=np.int32))
     return img_base, vec_base
-
 
 # def encode_image(image: PIL.Image.Image, clf: sklearn.cluster.KMeans) -> np.ndarray:
 #     """
@@ -42,14 +37,12 @@ def load_bases():
 #     emb, _ = np.histogram(_classes, len(clf.cluster_centers_), normed=True)
 #     return emb
 
-
 def encode_image(arr) -> np.ndarray:
     emds = []
     for i in arr:
         emb, _ = np.histogram(i, 2048, normed=True)
         emds.append(emb)
     return emds
-
 
 def find_indices_in_db(img, classifier, db, db_paths, db_neighbours):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -60,7 +53,6 @@ def find_indices_in_db(img, classifier, db, db_paths, db_neighbours):
     distances, indices = db_neighbours.kneighbors(predHist.reshape(1, -1), return_distance=True)
     return db_paths.loc[indices[0], ['img_path']].values, distances[0]
 
-
 def main():
     st.title('Our Super Search')
     up_file = st.file_uploader('Choice file', type=['jpeg', 'jpg', 'webp', 'png', 'tiff'])
@@ -69,7 +61,6 @@ def main():
             img = PIL.Image.open(up_file)
             img = PIL.ImageOps.exif_transpose(img)
             k = 5
-
             img_paths, dists = find_indices_in_db(np.array(img)[:, :, ::-1], loaded_model, database, path_database, db_neighbours)
             for i in range(len(img_paths)):
                 st.image(PIL.Image.open(img_paths[i][0]),
@@ -77,13 +68,11 @@ def main():
         except Exception as e:
             st.write('CRASHED:{}'.format(e))
 
-
 # main()
 loaded_model = load_model()
 path_database, database = load_bases()
 db_neighbours = NearestNeighbors(n_neighbors=5, metric='cosine')
 emdbs = encode_image(database['embedding'].values)
 db_neighbours.fit(emdbs)
-
 if __name__ == '__main__':
     main()
